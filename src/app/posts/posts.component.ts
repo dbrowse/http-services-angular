6175b28564bb2e857a0f950ab6a2e323bf2e8c9a
Diyan <dgeorgiev_job@yahoo.com>
2018-06-21 01:19:49 +0300

    Adding bootstrap and get , create , update methods to handle http service

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -6,18 +6,41 @@ import { Http } from '@angular/http';
   templateUrl: './posts.component.html',
   styleUrls: ['./posts.component.css']
 })
+
 export class PostsComponent implements OnInit {
+
    posts: any[];
-  constructor(http: Http) {
-    http.get('https://jsonplaceholder.typicode.com/posts')
+   private url = 'https://jsonplaceholder.typicode.com/posts';
+  constructor(private http: Http) {
+    http.get(this.url)
     .subscribe(response => {
-       this.posts = response.json();
+        this.posts = response.json();
+
+    });
+   }
+
+   createPost(input: HTMLInputElement) {
+    let post = { title: input.value };
+    input.value = '';
+
+    this.http.post(this.url, JSON.stringify(post))
+    .subscribe(response => {
+      post['id'] = response.json().id;
+      this.posts.splice(0, 0, post);
+
     });
 
    }
 
+   updatePost(post) {
+     this.http.patch(this.url + '/' + post.id, JSON.stringify({ isRead: true }))
+     .subscribe(response => {
+        console.log(response.json());
+     });
 
-  ngOnInit() {
-  }
+   }
+
+
+   ngOnInit() {}
 
 }
